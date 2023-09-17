04ef2122f868df80fc1efed2eebb57a0acc27a35
viniciusbdzn <viniciusbdzn@gmail.com>
2023-09-17 11:57:42 -0300

    imprimindo lista de convidados selecionados

--- src/candidatura/ProcessoSeletivo.java
+++ src/candidatura/ProcessoSeletivo.java
@@ -1,11 +1,53 @@
 package candidatura;
 
+import java.text.DecimalFormat;
+import java.util.concurrent.ThreadLocalRandom;
+
 public class ProcessoSeletivo {
 	public static void main(String[] args) {
+		/*
 		analisarCandidato(1900.0);
 		analisarCandidato(2300.0);
 		analisarCandidato(2000.0);
+		*/
+		
+		selecaoCandidatos();
 	}
+	static void selecaoCandidatos() {
+		String [] candidatos = {"LUIZ", "JOÃO", "MARIA", "ANA", "FÁBIO", "GABRIELA", "ROBERTO", "SÉRGIO", "ELISA", "DAVID"};
+		String [] listaCandidatosSelecionados = new String[5];
+		
+		int candidatosSelecionados = 0;
+		int candidatoAtual = 0;
+		double salarioBase = 2000.0;
+		while(candidatosSelecionados < 5 && candidatoAtual < candidatos.length) {
+			String candidato = candidatos[candidatoAtual];
+			double salarioPretendido = valorPretendido();
+			DecimalFormat df = new DecimalFormat("#.00");
+			
+			System.out.println("O candidato " + candidato + " solicitou esse valor de salário: R$" + df.format(salarioPretendido));
+			if(salarioBase >= salarioPretendido) {
+				System.out.println("O candidato " + candidato + " foi selecionado para a vaga.");
+				listaCandidatosSelecionados[candidatosSelecionados] = candidato;
+				candidatosSelecionados++;
+				
+			}
+			candidatoAtual++;
+			
+		}
+		System.out.println("Lista de candidatos selecionados abaixo:");
+		for(String candidatoSelecionado: listaCandidatosSelecionados) {
+			if(candidatoSelecionado != null)
+			System.out.println(candidatoSelecionado);
+		}
+		
+	}
+	
+	static double valorPretendido() {
+		return ThreadLocalRandom.current().nextDouble(1800, 2200);
+	}
+	
+	/*
 	static void analisarCandidato(double salarioPretendido) {
 		double salarioBase = 2000.0;
 		if(salarioBase > salarioPretendido) {
@@ -16,4 +58,5 @@ public class ProcessoSeletivo {
 			System.out.println("AGUARDANDO O RESULTADO DOS DEMAIS CANDIDATOS!!");
 		}
 	}
+	*/
 }
