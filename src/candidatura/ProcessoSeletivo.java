ca5003a2ac72003155e08794b0d44e024736a236
viniciusbdzn <viniciusbdzn@gmail.com>
2023-09-17 12:53:57 -0300

    algoritmo pra simular ligação para candidatos

--- src/candidatura/ProcessoSeletivo.java
+++ src/candidatura/ProcessoSeletivo.java
@@ -1,6 +1,7 @@
 package candidatura;
 
 import java.text.DecimalFormat;
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSeletivo {
@@ -15,11 +16,13 @@ public class ProcessoSeletivo {
 	}
 	static void selecaoCandidatos() {
 		String [] candidatos = {"LUIZ", "JOÃO", "MARIA", "ANA", "FÁBIO", "GABRIELA", "ROBERTO", "SÉRGIO", "ELISA", "DAVID"};
-		String [] listaCandidatosSelecionados = new String[5];
+		String [] listaCandidatosSelecionados = new String[5]; //array em que serão armazenados os candidatos selecionados
 		
 		int candidatosSelecionados = 0;
 		int candidatoAtual = 0;
 		double salarioBase = 2000.0;
+		
+		System.out.println("Salário pretendido pelos candidatos:");
 		while(candidatosSelecionados < 5 && candidatoAtual < candidatos.length) {
 			String candidato = candidatos[candidatoAtual];
 			double salarioPretendido = valorPretendido();
@@ -33,18 +36,52 @@ public class ProcessoSeletivo {
 				
 			}
 			candidatoAtual++;
-			
 		}
+		
+		//vai imprimir a lista de candidatos que serão chamados pra entrevista
+		System.out.println("---------------------------------------------------");
 		System.out.println("Lista de candidatos selecionados abaixo:");
 		for(String candidatoSelecionado: listaCandidatosSelecionados) {
 			if(candidatoSelecionado != null)
 			System.out.println(candidatoSelecionado);
 		}
-		
+		System.out.println("---------------------------------------------------");
+		System.out.println("Entrando em contato com os candidatos selecionados:");
+		for(String candidato: listaCandidatosSelecionados) {
+			if(candidato != null)
+			entrandoEmContato(candidato);
+		}
 	}
 	
+	//vai gerar um valor aleatório entre 1800 e 2200
 	static double valorPretendido() {
 		return ThreadLocalRandom.current().nextDouble(1800, 2200);
+	}
+	
+	static void entrandoEmContato(String candidato) {
+		int tentativasRealizadas = 1;
+		boolean continuarTentando = true;
+		boolean atendeu = false;
+		
+		do {
+			atendeu = atender();
+			continuarTentando = !atendeu;
+			if(continuarTentando)
+				tentativasRealizadas++;
+			else 
+				System.out.println("CONTATO REALIZADO COM SUCESSO!");
+		} while (continuarTentando && tentativasRealizadas < 3);
+		
+		if(atendeu)
+			System.out.println("CONSEGUIMOS CONTATO COM " + candidato + " NA " + tentativasRealizadas + "ª TENTATIVA.");
+		else {
+			System.out.println("NÃO CONSEGUIMOS CONTATO COM " + candidato + " NAS " + tentativasRealizadas + " TENTATIVAS REALIZADAS.");
+		}
+	}
+	
+	//método auxiliar simulando o atendimento da ligação
+	static boolean atender() {
+		return new Random().nextInt(3)==1;
 	}
 	
 	/*
