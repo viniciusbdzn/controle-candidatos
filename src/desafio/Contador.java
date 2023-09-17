742ee1d9e4ec8668fd6ab54591551d75a05b8d02
viniciusbdzn <viniciusbdzn@gmail.com>
2023-09-17 14:03:44 -0300

    criando o desafio contador da DIO

---
+++ src/desafio/Contador.java
@@ -0,0 +1,40 @@
+package desafio;
+
+import java.util.Scanner;
+
+public class Contador {
+	public static void main(String[] args) {
+		Scanner terminal = new Scanner(System.in);
+		System.out.println("Digite o primeiro parâmetro: ");
+		int parametroUm = terminal.nextInt();
+		System.out.println("Digite o segundo parâmetro: ");
+		int parametroDois = terminal.nextInt();
+		
+		try {
+			//chamando o método contendo a lógica de contagem
+			contar(parametroUm, parametroDois);
+		
+		}catch (ParametrosInvalidosException e) {
+			//imprimir a mensagem: O segundo parâmetro deve ser maior que o primeiro
+			System.out.println("O segundo parâmetro deve ser maior que o primeiro.");
+		}
+		terminal.close();
+	
+}
+	
+	static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
+		//validar se parametroUm é MAIOR que parametroDois e lançar a exceção
+		int contagem;
+		if(parametroUm > parametroDois) {
+			throw new ParametrosInvalidosException();
+		}
+		 else {
+			contagem = parametroDois - parametroUm;
+			for(int indice = 1; indice <= contagem; indice++) 
+				System.out.println(indice);
+		 }
+	}
+}
+		
+	
+
